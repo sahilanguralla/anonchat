250f0445a626f63951370283ed3d158919ad1ca2
Sahil Anguralla <anguralla.sahil@gmail.com>
2016-06-18 23:25:00 +0530

    bug fixed

--- public/js/room.js
+++ public/js/room.js
@@ -32,10 +32,7 @@ $(function() {
     } else {
       var pos = "right";
     }
-    var str = $('<div class="ui chat-message ' + pos + ' aligned ' + pos + ' floated ten wide column"><div class="ui comments"><div class="comment ' + pos + ' aligned ' + pos + ' floated ui grid"><div class="' + pos + ' floated two wide column"><a class="avatar"></div><div class="fourteen wide column"><div class="content"><a class="author"></a><div class="metadata"><div class="date"><!--2 days ago--></div></div><div class="text message-area"></div></div></div></div></div></div>');
-    str.find(".author").text(username);
-    str.find(".message-area").text(message);
-    str.find(".avatar").html('<img src="' + $("<div>").text(profilePic).html() +'" />');
+    var str = '<div class="ui chat-message ' + pos + ' aligned ' + pos + ' floated ten wide column"><div class="ui comments"><div class="comment ' + pos + ' aligned ' + pos + ' floated ui grid"><div class="' + pos + ' floated two wide column"><a class="avatar"><img src="' + text(profilePic).html() + '"></a></div><div class="fourteen wide column"><div class="content"><a class="author">' + text(username).html() + '</a><div class="metadata"><div class="date"><!--2 days ago--></div></div><div class="text">' + text(message).html() + '</div></div></div></div></div></div>';
     $('#message-feed').prepend(str).hide().slideDown(100);
   }
 
