c19254c5fa2c5c84daf001607779362cc3261c3a
Sahil Anguralla <anguralla.sahil@gmail.com>
2016-06-19 00:29:36 +0530

    jquery too on cdn

--- public/js/support.js
+++ public/js/support.js
@@ -2,6 +2,6 @@
 	var
 		eventSupport = ('querySelector' in document && 'addEventListener' in window)
 	jsonSupport = (typeof JSON !== 'undefined'),
-		jQuery = (eventSupport && jsonSupport) ? '/js/library/jquery.min.js' : '/js/library/jquery.legacy.min.js';
+		jQuery = (eventSupport && jsonSupport) ? 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js' : '/js/library/jquery.legacy.min.js';
 	document.write('<script src="' + jQuery + '"><\/script>');
 }());
