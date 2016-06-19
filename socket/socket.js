2cf975566ec405d7bed6fb2f93b0cf8342297918
Sahil Anguralla <anguralla.sahil@gmail.com>
2016-06-19 18:29:49 +0530

    Rooms not displaying bug

--- socket/socket.js
+++ socket/socket.js
@@ -2,7 +2,7 @@ module.exports = function(io, models) {
 	var chatrooms = io.of('/roomlist').on('connection', function(socket) {
 		console.log('Socket Connection established on Server Side!');
 		var Room = models.Room;
-		Room.all().exec(function(rooms) {
+		Room.find({}).exec(function(rooms) {
 			console.log("emmiting back all rooms", rooms);
 			socket.emit('room_update', JSON.stringify(rooms));
 		});
