399696b50f471f844ec7b37cceea0255ee421714
Sahil Anguralla <anguralla.sahil@gmail.com>
2016-06-19 13:06:09 +0530

    verifying domain

--- public/js/room.js
+++ public/js/room.js
@@ -54,4 +54,23 @@ $(function() {
       $('#user-feed').prepend(str).hide().slideDown(100);
     }
   })
+
+  // Registering Service Worker
+  var pushButton = $('.js-push-button');  
+  pushButton.click(function() {  
+    if (isPushEnabled) {  
+      unsubscribe();  
+    } else {  
+      subscribe();  
+    }  
+  });
+
+  // Check that service workers are supported, if so, progressively  
+  // enhance and add push messaging support, otherwise continue without it.  
+  if ('serviceWorker' in navigator) {  
+    navigator.serviceWorker.register('/service-worker.js')  
+    .then(initialiseState);  
+  } else {  
+    console.warn('Service workers aren\'t supported in this browser.');  
+  }  
 });
