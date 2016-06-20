a5c549ea4e0d28a947dacf475e861d310dce22d8
Sahil Anguralla <anguralla.sahil@gmail.com>
2016-06-20 12:47:18 +0530

    added body parser

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.bodyParser())
 // parse application/json
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 // detecting the environment
 var env = process.env.NODE_ENV || "development";
